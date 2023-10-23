cd42262ffad246c61c29c8fa597dbe0113c1b81d
tj <147413154+tomjmarz1@users.noreply.github.com>
2023-10-23 11:27:36 -0500

    ceil

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -8,24 +8,12 @@ describe('calculateNumber', function () {
   });
   it('should return result of adding rounded numbers', function () {
     assert.equal(calcul.calculateNumber(1, 3.7), 5);
-    assert.equal(calcul.calculateNumber(1.0, 0), 1);
-    assert.equal(calcul.calculateNumber(1.3, 0), 1);
-    assert.equal(calcul.calculateNumber(1.7, 0), 2);
   });
   it('should return result of adding rounded numbers', function () {
-    assert.equal(calcul.calculateNumber(1.2, 3.7), 5);
-    assert.equal(calcul.calculateNumber(0, 1.0), 1);
-    assert.equal(calcul.calculateNumber(0, 1.3), 1);
-    assert.equal(calcul.calculateNumber(0, 1.7), 2);
+    assert.equal(calcul.calculateNumber(1.0, 3.7), 5);
   });
   
   it('should return result of adding rounded numbers', function () {
     assert.equal(calcul.calculateNumber(1.5, 3.7), 6);
-    assert.equal(calcul.calculateNumber(1.3, 0), 1);
-    assert.equal(calcul.calculateNumber(0, 1.2), 1);
-    assert.equal(calcul.calculateNumber(1.3, 1.3), 2);
-    assert.equal(calcul.calculateNumber(1.7, 1.2), 3);
-    assert.equal(calcul.calculateNumber(1.3, 1.8), 3);
-    assert.equal(calcul.calculateNumber(1.6, 1.8), 4);
   });
 });
