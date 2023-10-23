c11fcb3fe75f6ea37fb73ea15e005bc375a626bc
tj <147413154+tomjmarz1@users.noreply.github.com>
2023-10-23 11:36:35 -0500

    edge case

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -15,5 +15,10 @@ describe('calculateNumber', function () {
   
   it('should return result of adding rounded numbers', function () {
     assert.equal(calcul.calculateNumber(1.5, 3.7), 6);
+    
+  });
+  it('should work with very large values', function() {
+    const result = calculateNumber(Number.MAX_SAFE_INTEGER, 0.4);
+    assert.equal(result, Number.MAX_SAFE_INTEGER);
   });
 });
