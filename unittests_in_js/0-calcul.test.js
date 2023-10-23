b3c3b71dfe50aeb79a25642a4e504bc01831e7b4
tj <147413154+tomjmarz1@users.noreply.github.com>
2023-10-23 11:20:02 -0500

    more tests

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -15,5 +15,11 @@ describe('calculateNumber', function () {
   
   it('should return result of adding rounded numbers', function () {
     assert.equal(calcul.calculateNumber(1.5, 3.7), 6);
+    assert.equal(calcul.calculateNumber(1.3, 0), 1);
+    assert.equal(calcul.calculateNumber(0, 1.2), 1);
+    assert.equal(calcul.calculateNumber(1.3, 1.3), 2);
+    assert.equal(calcul.calculateNumber(1.7, 1.2), 3);
+    assert.equal(calcul.calculateNumber(1.3, 1.8), 3);
+    assert.equal(calcul.calculateNumber(1.6, 1.8), 4);
   });
 });
