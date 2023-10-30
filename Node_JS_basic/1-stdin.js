8c8913340d5e43477a99523ec9671116fd142e4f
tj <147413154+tomjmarz1@users.noreply.github.com>
2023-10-30 16:20:49 -0500

    remove the import

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,12 @@
-const {stdin, stdout} = require('node:process');
+process.stdout.write("Welcome to Holberton School, what is your name?\n")
 
-stdout.write("Welcome to Holberton School, what is your name?\n")
-
-stdin.on('readable', () =>{
-    const name = stdin.read();
+process.stdin.on('readable', () =>{
+    const name = process.stdin.read();
     if(name){
-        stdout.write("Your name is: " + name);
+        process.stdout.write("Your name is: " + name);
     }
 })
 
-stdin.on('end', () => {
-    stdout.write('This important software is now closing\n');
+process.stdin.on('end', () => {
+   process.stdout.write('This important software is now closing\n');
 })
