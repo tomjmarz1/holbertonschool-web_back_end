f2f2574649470f8f8e6e5c64a359ee30100926f5
tj <147413154+tomjmarz1@users.noreply.github.com>
2023-10-30 16:07:50 -0500

    fdf

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,13 @@
 const {stdin, stdout} = require('node:process');
 
-stdout.write("Welcome to Holberton School, what is your name?")
-let name;
+stdout.write("Welcome to Holberton School, what is your name?\n")
+
 stdin.on('readable', () =>{
-    name = stdin.read();
-    stdout.write("Your name is: " + name + "\nThis important software is now closing");
-    
+    const name = stdin.read();
+    if(name){
+        stdout.write("Your name is: " + name);
+    }
 })
-
-
 
 stdin.on('end', () => {
     stdout.write('This important software is now closing\n');
